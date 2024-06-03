c5bf5d529acc4b1ac8e647ae171e03a1ab1f3531
ВАШÐAnastasia <anastasija2003@mail.ru>
2024-06-03 17:52:08 +0300

    HW18

---
+++ app/src/main/java/com/example/hw18/presentation/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.hw18.presentation
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hw18.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
